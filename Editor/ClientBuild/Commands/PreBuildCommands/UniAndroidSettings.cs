acf0736c0699963dc25e9c41e60292c72e085530
Mefodei <mefodei.link@gmail.com>
2020-06-23 16:32:14 +0300

    - update menu items

---
+++ Editor/ClientBuild/Commands/PreBuildCommands/UniAndroidSettings.cs
@@ -0,0 +1,25 @@
+﻿namespace UniModules.UniGame.UniBuild.Editor.ClientBuild.Commands.PreBuildCommands
+{
+    using System;
+    using UnityEditor;
+
+    [Serializable]
+    public class UniAndroidSettings
+    {
+        public AndroidBuildType AndroidBuildType = AndroidBuildType.Development;
+
+        public AndroidArchitecture AndroidArchitecture = AndroidArchitecture.ARMv7;
+
+        public ApiCompatibilityLevel ApiCompatibilityLevel = ApiCompatibilityLevel.NET_4_6;
+        
+        public bool BuildAppBundle = false;
+
+        public bool AllowDebugging = false;
+        
+        public bool IsDevelopment = false;
+
+        public bool ExportAsGoogleAndroidProject;
+
+        public ScriptingImplementation ScriptingBackend = ScriptingImplementation.Mono2x;
+    }
+}
