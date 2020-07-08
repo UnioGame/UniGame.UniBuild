b5a168d60fc0a5bb805baded228b0637eab6eb50
Mefodei <mefodei.link@gmail.com>
2020-07-08 13:16:31 +0300

    - add switch build target command

---
+++ Editor/ClientBuild/Commands/PreBuildCommands/SwitchActiveBuildTargetCommand.cs
@@ -0,0 +1,20 @@
+﻿using UniModules.UniGame.UniBuild.Editor.ClientBuild.Interfaces;
+
+namespace UniModules.UniGame.UniBuild.Editor.ClientBuild.Commands.PreBuildCommands
+{
+    using UnityEditor;
+    using UnityEngine;
+
+    [CreateAssetMenu(menuName = "UniGame/UniBuild/Commands/Switch Active Build Target", fileName = nameof(SwitchActiveBuildTargetCommand))]
+    public class SwitchActiveBuildTargetCommand : UnityPreBuildCommand
+    {
+        
+        public BuildTargetGroup BuildTargetGroup = BuildTargetGroup.Android;
+        public BuildTarget BuildTarget = BuildTarget.Android;
+        
+        public override void Execute(IUniBuilderConfiguration buildParameters)
+        {
+            EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTargetGroup,BuildTarget);
+        }
+    }
+}
