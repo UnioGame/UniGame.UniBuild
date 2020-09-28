b4551275d5bd45c086c65f2db94fae2a4e808798
Mefodei <mefodei.link@gmail.com>
2020-09-28 23:12:47 +0300

    - add asset reimport command

---
+++ Editor/ClientBuild/Commands/PreBuildCommands/AssetsCommands/ReimportAssetsCommand.cs
@@ -0,0 +1,29 @@
+﻿using UniModules.UniGame.UniBuild.Editor.ClientBuild.Interfaces;
+
+namespace UniModules.UniGame.UniBuild.Editor.ClientBuild.Commands.PreBuildCommands.AssetsCommands
+{
+    using System.Collections.Generic;
+    using UnityEditor;
+    using UnityEngine;
+
+    /// <summary>
+    /// reimport target assets locations
+    /// or target assets
+    /// </summary>
+    public class ReimportAssetsCommand : UnitySerializablePreBuildCommand
+    {
+        public List<Object> assets = new List<Object>();
+
+        public ImportAssetOptions ImportAssetOptions = ImportAssetOptions.ImportRecursive;
+        
+        public override void Execute(IUniBuilderConfiguration buildParameters)
+        {
+            foreach (var asset in assets)
+            {
+                if(!asset) continue;
+                var assetPath = AssetDatabase.GetAssetPath(asset);
+                AssetDatabase.ImportAsset(assetPath, ImportAssetOptions);
+            }       
+        }
+    }
+}
