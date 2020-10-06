ee7ac4994596615047de0df323e277c6d04d2496
Mefodei <mefodei.link@gmail.com>
2020-10-07 01:57:09 +0300

    - update package

---
+++ Editor/ClientBuild/Commands/PreBuildCommands/DefineSymbolsAssetCommand.cs
@@ -0,0 +1,26 @@
+﻿namespace UniModules.UniGame.UniBuild.Editor.ClientBuild.Commands.PreBuildCommands
+{
+    using System;
+    using Interfaces;
+    using UnityEngine;
+
+    [Serializable]
+    [CreateAssetMenu(menuName = "UniGame/UniBuild/DefineSymbolsAssetCommand",fileName = nameof(DefineSymbolsAssetCommand))]
+    public class DefineSymbolsAssetCommand : UnityPreBuildCommand
+    {
+        [SerializeField]
+#if ODIN_INSPECTOR
+        [Sirenix.OdinInspector.InlineProperty]
+        [Sirenix.OdinInspector.HideLabel]
+#endif
+        private ApplyScriptingDefineSymbolsCommand _command = new ApplyScriptingDefineSymbolsCommand();
+
+        public override void Execute(IUniBuilderConfiguration configuration)
+        {
+            _command.Execute(configuration);
+        }
+
+        public void Execute() => _command.Execute(string.Empty);
+        
+    }
+}
