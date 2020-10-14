19499bd824ebc5f15dbd37509e2c5c9ec2990963
Mefodei <mefodei.link@gmail.com>
2020-10-14 19:09:25 +0300

    - add shared commands asset

---
+++ Editor/ClientBuild/Commands/SharedCommands.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using UniModules.UniGame.UniBuild.Editor.ClientBuild.BuildConfiguration;
+using UniModules.UniGame.UniBuild.Editor.ClientBuild.Commands.PreBuildCommands;
+using UniModules.UniGame.UniBuild.Editor.ClientBuild.Interfaces;
+using UnityEngine;
+
+namespace UniModules.UniGame.UniBuild.Editor.ClientBuild.Commands
+{
+    [Serializable]
+    [CreateAssetMenu(menuName = "UniGame/UniBuild/SharedCommands",fileName = nameof(SharedCommands))]
+    public class SharedCommands : UnityBuildCommand
+    {
+        
+        public List<BuildCommandStep> commands = new List<BuildCommandStep>();
+
+        public override void Execute(IUniBuilderConfiguration configuration)
+        {
+            foreach (var command in commands)
+            {
+                foreach (var buildCommand in command.GetCommands())
+                {
+                    if (!buildCommand.IsActive)
+                    {
+                        continue;
+                    }
+                    buildCommand.Execute(configuration);
+                }
+            }
+        }
+    }
+}
