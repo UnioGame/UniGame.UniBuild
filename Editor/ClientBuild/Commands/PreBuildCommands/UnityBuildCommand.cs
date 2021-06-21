20ab17b213fb5e6831e21093c29c16311e89f8ac
MikhailovSergei <mikhailovs@taktikagames.ru>
2021-06-21 18:52:03 +0300

    update packages

--- Editor/ClientBuild/Commands/PreBuildCommands/UnityBuildCommand.cs
+++ Editor/ClientBuild/Commands/PreBuildCommands/UnityBuildCommand.cs
@@ -10,13 +10,23 @@
     public abstract class UnityBuildCommand : ScriptableObject,IUnityBuildCommand
     {
         [SerializeField]
+        public string commandName = String.Empty;
+        
+        [SerializeField]
         public bool _isActive = true;
 
         public bool IsActive => _isActive;
-        public string Name => name;
+        
+        public string Name => commandName;
         
         public abstract void Execute(IUniBuilderConfiguration configuration);
 
         public virtual bool Validate(IUniBuilderConfiguration config) => _isActive;
+
+
+        public virtual void OnValidate()
+        {
+            commandName = string.IsNullOrEmpty(commandName) ? name : commandName;
+        }
     }
 }
