0ce6b8dc435aeec2fb4dc4714ac538d12c58ad49
MikhailovSergei <mikhailovs@taktikagames.ru>
2021-10-07 13:57:37 +0300

    - update build configuration window

---
+++ Editor/Windows/UniBuildConfigurationAsset.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using UniModules.Editor;
+using UniModules.UniGame.UniBuild.Editor.ClientBuild.BuildConfiguration;
+using UnityEngine;
+
+public class UniBuildConfigurationAsset : ScriptableObject
+{
+    #region public properties
+        
+        
+    [Searchable]
+    [ListDrawerSettings(Expanded = true)]
+    [InlineEditor]
+    public List<UniBuildCommandsMap> configurations = new List<UniBuildCommandsMap>();
+
+    #endregion
+    
+    [Button]
+    public void Refresh()
+    {
+        configurations.Clear();
+        var configs = AssetEditorTools.GetAssets<UniBuildCommandsMap>();
+        configurations.AddRange(configs);
+    }
+
+    private void OnEnable()
+    {
+        Refresh();
+    }
+}
