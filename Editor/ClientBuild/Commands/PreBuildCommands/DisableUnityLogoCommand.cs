64451f55ecbcdff26b0dd7b3f32ed64c79db44e7
Mefodei <mefodei.link@gmail.com>
2021-10-30 15:48:04 +0300

    - update build commands

--- Editor/ClientBuild/Commands/PreBuildCommands/DisableUnityLogoCommand.cs
+++ Editor/ClientBuild/Commands/PreBuildCommands/DisableUnityLogoCommand.cs
@@ -1,13 +1,22 @@
-﻿namespace UniModules.UniGame.UniBuild.Editor.ClientBuild.Commands.PreBuildCommands {
+﻿using UniModules.UniGame.Core.Runtime.Interfaces;
+
+namespace UniModules.UniGame.UniBuild.Editor.ClientBuild.Commands.PreBuildCommands {
     using System;
     using Interfaces;
     using UnityEditor;
 
     [Serializable]
-    public class DisableUnityLogoCommand : UnitySerializablePreBuildCommand
+    public class DisableUnityLogoCommand : UnitySerializablePreBuildCommand, ICommand
     {
-        public override void Execute(IUniBuilderConfiguration buildParameters) {
+        public override void Execute(IUniBuilderConfiguration buildParameters) => Execute();
+
+#if ODIN_INSPECTOR
+        [Sirenix.OdinInspector.Button]
+#endif
+        public void Execute()
+        {
             PlayerSettings.SplashScreen.showUnityLogo = false;
         }
+        
     }
 }
--- Editor/ClientBuild/Commands/PreBuildCommands/SerializableBuildCommand.cs
+++ Editor/ClientBuild/Commands/PreBuildCommands/SerializableBuildCommand.cs
@@ -1,4 +1,6 @@
-﻿namespace UniModules.UniGame.UniBuild.Editor.ClientBuild.Commands.PreBuildCommands
+﻿using UniModules.UniGame.Core.Runtime.Interfaces;
+
+namespace UniModules.UniGame.UniBuild.Editor.ClientBuild.Commands.PreBuildCommands
 {
     using System;
     using Interfaces;
@@ -13,12 +15,11 @@
         public string name;
         
         public bool IsActive => isActive;
-        
+
         public string Name => string.IsNullOrEmpty(name) ? this.GetType().Name : name;
         
         public virtual bool Validate(IUniBuilderConfiguration config) => isActive;
         
         public abstract void Execute(IUniBuilderConfiguration buildParameters);
-
     }
 }
