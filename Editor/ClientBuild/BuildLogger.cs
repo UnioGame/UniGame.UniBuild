0091d23cbc93568810b16939737c99e645ed3be8
Mefodei <mefodei.link@gmail.com>
2022-01-20 00:20:59 +0300

    - update android build configuration settings command

--- Editor/ClientBuild/BuildLogger.cs
+++ Editor/ClientBuild/BuildLogger.cs
@@ -1,25 +1,34 @@
-﻿using System.Text;
+﻿using System;
+using System.IO;
+using System.Text;
+using UniModules.Editor;
 using UnityEngine;
 
 namespace UniModules.UniGame.UniBuild.Editor.ClientBuild
 {
     public static class BuildLogger
     {
-        private const string MessageFormat = "=== UNIBUILD : {0}\n";
+        private const string MessageFormat = "=== UNIBUILD [{0}] : {1}\n";
+        private static string BuildDirectory = "Builds".ToAbsoluteProjectPath();
+        private static string BuildLogPath = BuildDirectory.CombinePath("last_build_log.log");
+        private static string BuildStartMessage = "=== UNIBUILD BUILD REPORT ===\n\n";
         
         private static StringBuilder _buildLog = new StringBuilder();
 
         public static void Initialize()
         {
+            
             _buildLog ??= new StringBuilder(); 
             _buildLog.Clear();
-            _buildLog.AppendLine("=== UNIBUILD BUILD REPORT ===\n\n");
+            _buildLog.AppendLine(BuildStartMessage);
+
+            if (!Directory.Exists(BuildDirectory))
+                Directory.CreateDirectory(BuildDirectory);
+
+            File.WriteAllText(BuildLogPath,BuildStartMessage);
         }
 
-        public static void Finish()
-        {
-            _buildLog?.Clear();
-        }
+        public static void Finish() =>  _buildLog?.Clear();
         
         public static void Log(string log)
         {
@@ -31,13 +40,13 @@ namespace UniModules.UniGame.UniBuild.Editor.ClientBuild
         {
             if (string.IsNullOrEmpty(log))
                 return;
-            _buildLog.AppendFormat(MessageFormat,log);
+
+            var message = string.Format(MessageFormat,DateTime.Now, log);
+            _buildLog.AppendLine(message);
+            File.AppendAllText(BuildLogPath,message);
         }
 
-        public static void Print()
-        {
-            Debug.Log(_buildLog);
-        }
+        public static void Print() => Debug.Log(_buildLog);
         
     }
 }
