ebfedfb7a3e826e91cdba7c568b38eb08bb9e36d
Mefodei <mefodei.link@gmail.com>
2024-01-13 12:51:49 +0700

    - fix dependency errors

--- Editor/Windows/UniBuildItem.cs
+++ Editor/Windows/UniBuildItem.cs
@@ -1,17 +1,26 @@
 ﻿using System;
-using Sirenix.OdinInspector;
 using UniModules.UniGame.UniBuild.Editor.ClientBuild.BuildConfiguration;
 
+#if ODIN_INSPECTOR
+using Sirenix.Utilities.Editor;
+#endif
+
 [Serializable]
+#if ODIN_INSPECTOR
 [InlineProperty]
 [HideLabel]
+#endif
 public class UniBuildItem
 {
+#if ODIN_INSPECTOR
     [HorizontalGroup(nameof(UniBuildItem))]
     [InlineEditor()]
+#endif
     public UniBuildCommandsMap buildCommands;
 
+#if ODIN_INSPECTOR
     [Button]
+#endif
     public void Build()
     {
         buildCommands.ExecuteBuild();
