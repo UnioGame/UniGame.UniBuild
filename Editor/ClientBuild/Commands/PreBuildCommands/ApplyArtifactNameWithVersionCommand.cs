0e673706bc66565b3b1814f88c429e1ad9593130
mefodei <mefodei.link@gmail.com>
2024-05-22 18:14:37 +0700

    - update artifact name command

--- Editor/ClientBuild/Commands/PreBuildCommands/ApplyArtifactNameWithVersionCommand.cs
+++ Editor/ClientBuild/Commands/PreBuildCommands/ApplyArtifactNameWithVersionCommand.cs
@@ -4,45 +4,76 @@
     using System.IO;
     using GitTools.Runtime;
     using global::UniGame.UniBuild.Editor.ClientBuild.Interfaces;
-    using Interfaces;
     using UnityEditor;
     using UnityEngine;
 
+#if ODIN_INSPECTOR
+    using Sirenix.OdinInspector;
+#endif
+
+#if TRI_INSPECTOR
+    using TriInspector;
+#endif
+    
     [Serializable]
     public class ApplyArtifactNameWithVersionCommand : UnitySerializablePreBuildCommand
     {
         private const string nameFormatTemplate = "{0}-{1}";
 
+#if  ODIN_INSPECTOR || TRI_INSPECTOR
+        [BoxGroup(nameof(artifactName))]
+#endif
+        public bool overrideArtifactName = false;
+        
+#if  ODIN_INSPECTOR || TRI_INSPECTOR
+        [ShowIf(nameof(overrideArtifactName))]
+        [BoxGroup(nameof(artifactName))]
+#endif
         public bool useProductName = true;
+        
+#if  ODIN_INSPECTOR || TRI_INSPECTOR
+        [BoxGroup(nameof(artifactName))]
+        [HideIf(nameof(useProductName))]
+#endif
+        public string artifactName = string.Empty;
+        
         public bool includeGitBranch;
         public bool includeBundleVersion = true;
         public bool useNameTemplate = false;
 
         public string artifactNameTemplate = string.Empty;
-
+        
+        public bool overrideExtension = false;
+        
+#if  ODIN_INSPECTOR || TRI_INSPECTOR
+        [ShowIf(nameof(overrideExtension))]
+#endif
         [Header("Optional: Extension: use '.' before file extension")]
         public string artifactExtension = "";
 
         public override void Execute(IUniBuilderConfiguration buildParameters)
         {
             var outputFilename = buildParameters.BuildParameters.outputFile;
-            var artifactName = CreateArtifactLocation(outputFilename, PlayerSettings.productName);
-            buildParameters.BuildParameters.outputFile = artifactName;
+            var outputName = CreateArtifactLocation(outputFilename, PlayerSettings.productName);
+            buildParameters.BuildParameters.outputFile = outputName;
         }
 
         public string CreateArtifactLocation(string outputFilename, string productName)
         {
-            var outputExtension = string.IsNullOrEmpty(artifactExtension)
-                    ? GetExtension()
-                    : artifactExtension;
-
+            var outputExtension = string.IsNullOrEmpty(artifactExtension) || 
+                                  !overrideExtension
+                ? GetExtension()
+                : artifactExtension;
+            
             var fileName = Path.GetFileNameWithoutExtension(outputFilename);
-
-            var artifactName = useProductName ? productName : fileName;
+            
+            var outputName = overrideArtifactName 
+                ? useProductName ? productName : artifactName 
+                : fileName;
 
             if (useNameTemplate)
             {
-                artifactName = string.Format(artifactNameTemplate, artifactName);
+                outputName = string.Format(artifactNameTemplate, outputName);
             }
 
             if (includeGitBranch)
@@ -50,19 +81,19 @@
                 var branch = GitCommands.GetGitBranch();
                 if (string.IsNullOrEmpty(branch) == false)
                 {
-                    artifactName = string.Format(nameFormatTemplate, artifactName, branch);
+                    outputName = string.Format(nameFormatTemplate, outputName, branch);
                 }
             }
 
             if (includeBundleVersion)
             {
-                artifactName = string.Format(nameFormatTemplate, artifactName, PlayerSettings.bundleVersion);
+                outputName = string.Format(nameFormatTemplate, outputName, PlayerSettings.bundleVersion);
             }
 
-            artifactName = artifactName.Replace(":", "");
-            artifactName += $"{outputExtension}";
+            outputName = outputName.Replace(":", "");
+            outputName += $"{outputExtension}";
 
-            return artifactName;
+            return outputName;
         }
 
         private string GetExtension()
