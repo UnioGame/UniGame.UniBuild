e8e1151c4037bdeeb5bfeb52899bdc700f0074ae
Mefodei <mefodei.link@gmail.com>
2024-08-19 16:30:06 +0700

    - update namespace

--- Editor/ClientBuild/Generator/BuildConfigurationBuilder.cs
+++ Editor/ClientBuild/Generator/BuildConfigurationBuilder.cs
@@ -9,12 +9,12 @@ namespace UniModules.UniGame.UniBuild.Editor.ClientBuild.Generator
     public class BuildConfigurationBuilder
     {
         private static string _cloudLocalPath = "UniBuild/Editor/" + CloudBuildMethodsGenerator.ClassFileName;
-        private static string _cloudPath = EditorFileUtils.Combine(EditorPathConstants.GeneratedContentPath,_cloudLocalPath);
+        private static string _cloudPath = FileUtils.Combine(EditorPathConstants.GeneratedContentPath,_cloudLocalPath);
 
         private static string _menuScript  = string.Empty;
         private static string _cloudScript = string.Empty;
 
-        public static string BuildPath => EditorFileUtils.Combine(EditorPathConstants.GeneratedContentPath,"UniBuild/Editor/BuildMethods.cs");
+        public static string BuildPath => FileUtils.Combine(EditorPathConstants.GeneratedContentPath,"UniBuild/Editor/BuildMethods.cs");
         
         [MenuItem("UniGame/Uni Build/Rebuild Menu")]
         public static void RebuildMenuAction()
