d182e8fa3e80a5d8b1e639703015c3b525bf9f5d
Mefodei <mefodei.link@gmail.com>
2024-11-25 18:50:51 +0700

    - add open script button

--- Editor/ClientBuild/Commands/PreBuildCommands/SerializableBuildCommand.cs
+++ Editor/ClientBuild/Commands/PreBuildCommands/SerializableBuildCommand.cs
@@ -3,11 +3,19 @@
     using System;
     using global::UniGame.UniBuild.Editor.ClientBuild.Interfaces;
     using Interfaces;
+    using Sirenix.OdinInspector;
+    using UniModules.Editor;
     using UnityEngine;
 
     [Serializable]
     public abstract class SerializableBuildCommand : IUnityBuildCommand
     {
+#if ODIN_INSPECTOR
+        [InlineButton(nameof(OpenScript),SdfIconType.Folder2Open)]
+#endif
+#if TRI_INSPECTOR || ODIN_INSPECTOR
+        [GUIColor("GetButtonColor")]
+#endif
         [SerializeField]
         public bool isActive = true;
         
@@ -18,5 +26,21 @@
         public virtual bool Validate(IUniBuilderConfiguration config) => isActive;
         
         public abstract void Execute(IUniBuilderConfiguration buildParameters);
+        
+#if TRI_INSPECTOR
+        [Button]
+#endif
+        public void OpenScript()
+        {
+            GetType().OpenScript();
+        }
+        
+        private Color GetButtonColor()
+        {
+            return isActive ? 
+                new Color(0.2f, 1f, 0.2f) : 
+                new Color(1, 0.6f, 0.4f);
+            return Color.green;
+        }
     }
 }
